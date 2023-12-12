74c7612eb24474d76b8508d1e9676334563b6f08
Song0-0 <ks08094@naver.com>
2023-12-12 22:42:11 +0900

    싱글톤 방식의 주의점 - 필드 공유 문제 해결

--- src/test/java/hello/core/singleton/StatefulServiceTest.java
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -5,8 +5,6 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 class StatefulServiceTest {
     @Test
     void statefulServiceSingleton() {
@@ -15,15 +13,14 @@ class StatefulServiceTest {
         StatefulService statefulService2 = ac.getBean(StatefulService.class);
 
         //ThreadA : A사용자가 10,000원 주문
-        statefulService1.order("userA", 10000);
+        int userAPrice = statefulService1.order("userA", 10000);
         //ThreadB : B사용자가 20,000원 주문
-        statefulService2.order("userB", 20000);
+        int userBPrice = statefulService2.order("userB", 20000);
 
         //ThreadA : 사용자 A가 주문 금액 조회
-        int price = statefulService1.getPrice();
+        int price = userAPrice;
         System.out.println("price = " + price);
 
-        assertThat(statefulService1.getPrice()).isEqualTo(20000);
     }
 
     static class TestConfig {
