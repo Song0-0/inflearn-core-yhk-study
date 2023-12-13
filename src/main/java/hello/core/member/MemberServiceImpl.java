d556873cdafe2c6a28d2021b142d55bc02e58521
Song0-0 <ks08094@naver.com>
2023-12-13 22:01:27 +0900

    설정파일에 따로 기재하지 않고, @Component등록된 클래스들이 빈으로 등록되게 하기, 의존성 주입은 @Autowired

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,8 +1,13 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService {
     private final MemberRepository memberRepository;
 
+    @Autowired //의존관계 자동으로 주입 ac.getBean(MemberRepository.class)
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
